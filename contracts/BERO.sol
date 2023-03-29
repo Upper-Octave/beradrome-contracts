26625d836ef41e37485206939e966f1666099ed8
issawayzani <38286051+issawayzani@users.noreply.github.com>
2023-03-30 02:17:49 +0300

    Update BERO.sol

--- contracts/BERO.sol
+++ contracts/BERO.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.13;
 
-import "contracts/interfaces/IFlow.sol";
+import "contracts/interfaces/IBERO.sol";
 
 contract BERO is IBERO {
     string public constant name = "Beradrome";
